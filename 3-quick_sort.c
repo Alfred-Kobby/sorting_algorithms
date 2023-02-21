97f2aae99b013e3b7f6a05dd34c4ae725efc5945
Alfred Tettey Ternor <aternor@itconsortiumgh.com>
2023-02-21 20:26:53 +0000

    debugging

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -5,10 +5,10 @@
 
 #include "sort.h"
 
-void swap_ints(int *a, int *b);
-int lomuto_partition(int *array, size_t size, int left, int right);
-void lomuto_sort(int *array, size_t size, int left, int right);
-void quick_sort(int *array, size_t size);
+// void swap_ints(int *a, int *b);
+// int lomuto_partition(int *array, size_t size, int left, int right);
+// void lomuto_sort(int *array, size_t size, int left, int right);
+// void quick_sort(int *array, size_t size);
 
 /**
  * swap_ints - Swap two integers in an array.
